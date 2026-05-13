67026a3c3e3d678fa6889ab4c7dc51a96dad1681
Samah Alsabbagh <samah.alsabbaghs@gmail.com>
2026-05-13 15:36:46 +0300

    Comments Deletion

--- backend/WazaranPI.Api/Repositories/Interfaces/IReportRepository.cs
+++ backend/WazaranPI.Api/Repositories/Interfaces/IReportRepository.cs
@@ -12,18 +12,3 @@ namespace WazaranPI.Api.Repositories.Interfaces
     }
 }
  
-//okay so now i have this new repoer, go ahead and do the following
-    //create the full backend flow :
-    // controler / sales / salesvariance / controller.cs
-    // repo / interface / sales /salesvariance / .cs
-    // repo / sales / salesvariance / .cs
-    // and so on 
-// front end page : load data in table, if possible barchar to show the diff for each sales man
-// the diff column showd be sortable, when i click i sort DESC / ASEC front end only for performance
-// export excel 
-// print pdf
-// follow thw design used in the page i sent you
-// use pagination or what evev that is performane killer 
-// tell what files to crete / where, page.tsx i send you should be replaced (keep the things that dont need to be changed like the title bar)
-// design well
-// explain each part when you finish 
